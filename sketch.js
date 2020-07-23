a5756c60777a902b800d9b8f29125a3f4e86e994
nimishrai <66766634+nimishrai@users.noreply.github.com>
2020-07-23 12:41:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var box1,box2,box3
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255.255,255)
+        
+        box1Sprite=createSprite(400,650,200,20);
+	box1Sprite.shapeColor=color(255,0,0)
+        box2Sprite=createSprite(495,610,20,100);
+	box2Sprite.shapeColor=color(255,0,0)
+        box3Sprite=createSprite(305,610,20,100);
+	box3Sprite.shapeColor=color(255,0,0)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+        box1 = Bodies.rectangle(400,650,200,50, {isStatic:true} );
+        World.add(world, box1);
+        box2 = Bodies.rectangle(495,610,20,100, {isStatic:true} );
+        World.add(world, box2);
+        box3 = Bodies.rectangle(305,610,20,100, {isStatic:true} );
+        World.add(world, box3);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  Engine.update(engine);
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    }
+}
+
+
+
